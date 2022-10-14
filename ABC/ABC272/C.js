434a35362c3287a2d7278a39a0bde74794af7b23
ponmas <teknoh10322@gmail.com>
2022-10-14 21:55:23 +0900

    abc272

---
+++ ABC/ABC272/C.js
@@ -0,0 +1,24 @@
+//B-Everyone is Friends.js
+
+'use strict';
+
+const main = (arg) => {
+  //入力
+  const input = arg.trim().split(/\r?\n/);
+  const n = input[0].split(' ').map((str) => parseInt(str, 10));
+  const a = input[0].split(' ').map((str) => parseInt(str, 10));
+  let 1 = -1;
+  let ans2 = -1;
+
+  for(let i =0; i<n;i++){
+    if()
+  }
+
+
+};
+
+//提出用
+//main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+
+//テスト用
+main(require('fs').readFileSync('test.txt', 'utf8'));
